818beab360b9fc9a23f8891f9f114bb5d042667e
Ryan O’Boyle <durbanpoison@protonmail.com>
2022-08-28 14:59:55 -0700

    first commit

---
+++ src/constants/endpoints.js
@@ -0,0 +1,10 @@
+const endpoints = {
+  navbar: 'profile/navbar.json',
+  routes: 'profile/routes.json',
+  home: 'profile/home.json',
+  about: 'profile/about.json',
+  projects: 'profile/projects.json',
+  inDevelopment: 'profile/inDevelopment.json',
+};
+
+export default endpoints;
